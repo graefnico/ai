96acdb55609e4a5380440459ffe02126c955c282
Nico Gräf <contact@nicograef.de>
2017-02-04 20:40:57 +0100

    improvement of neural net

--- neural_net/NeuralNetworkTest.py
+++ neural_net/NeuralNetworkTest.py
@@ -2,81 +2,19 @@ from NeuralNetwork import NeuralNetwork
 import numpy as np
 import matplotlib.pyplot as plt
 
-# input test gridset; just the first 32 numbers in binary
-X2 = np.array([[0,0,0,0,0],
-                    [0,0,0,0,1],
-                    [0,0,0,1,0],
-                    [0,0,0,1,1],
-                    [0,0,1,0,0],
-                    [0,0,1,0,1],
-                    [0,0,1,1,0],
-                    [0,0,1,1,1],
-                    [0,1,0,0,0],
-                    [0,1,0,0,1],
-                    [0,1,0,1,0],
-                    [0,1,0,1,1],
-                    [0,1,1,0,0],
-                    [0,1,1,0,1],
-                    [0,1,1,1,0],
-                    [0,1,1,1,1],
-                    [1,0,0,0,0],
-                    [1,0,0,0,1],
-                    [1,0,0,1,0],
-                    [1,0,0,1,1],
-                    [1,0,1,0,0],
-                    [1,0,1,0,1],
-                    [1,0,1,1,0],
-                    [1,0,1,1,1],
-                    [1,1,0,0,0],
-                    [1,1,0,0,1],
-                    [1,1,0,1,0],
-                    [1,1,0,1,1],
-                    [1,1,1,0,0],
-                    [1,1,1,0,1],
-                    [1,1,1,1,0],
-                    [1,1,1,1,1]])
+delta = 20 * (2*np.random.random((30,2))-1).round(1)
+X1_blue = np.concatenate((80 + delta, [[5,50],[70,20],[65,10],[75,15],[10,40],[20,10],[10,20],[10,10],[10,60],[10,70],[40,90],[90,50],[20,90],[90,10],[75,2]])).T
+X1_red = np.concatenate((40 + delta, [[30,15],[15,50],[10,90],[13,80],[20,80],[40,5],[50,15],[30,70],[40,80],[50,70],[80,40],[90,40],[90,20],[80,30],[90,30],[75,45]])).T
 
-# output test gridset; counting the ones of the input in binary
-Y2 = np.array([[0,0,0],
-                    [0,0,1],
-                    [0,0,1],
-                    [0,1,0],
-                    [0,0,1],
-                    [0,1,0],
-                    [0,1,0],
-                    [0,1,1],
-                    [0,0,1],
-                    [0,1,0],
-                    [0,1,0],
-                    [0,1,1],
-                    [0,1,0],
-                    [0,1,1],
-                    [0,1,1],
-                    [1,0,0],
-                    [0,0,1],
-                    [0,1,0],
-                    [0,1,0],
-                    [0,1,1],
-                    [0,1,0],
-                    [0,1,1],
-                    [0,1,1],
-                    [1,0,0],
-                    [0,1,0],
-                    [0,1,1],
-                    [0,1,1],
-                    [1,0,0],
-                    [0,1,1],
-                    [1,0,0],
-                    [1,0,0],
-                    [1,0,1]])
+X1_blue = X1_blue / 100
+X1_red = X1_red / 100
 
+Y1_0 = np.array([[i[0],j[0]] for i,j in zip(np.zeros((len(X1_blue.T),1)), np.ones((len(X1_blue.T),1)))])
+Y1_1 = np.array([[i[0],j[0]] for i,j in zip(np.ones((len(X1_red.T),1)), np.zeros((len(X1_red.T),1)))])
 
-delta = 0.2 * (2*np.random.random((30,2))-1).round(2)
-X1_0 = np.concatenate((0.8 + delta, [[.05,.5],[.65,.25],[.1,.4],[.2,.2],[.2,.1],[.1,.2],[.1,.1],[.1,.6],[.1,.7],[.4,.9],[.95,.4],[.2,.9],[.9,.1],[.75,.2]])).T
-X1_1 = np.concatenate((0.4 + delta, [[.15,.5],[.1,.9],[.2,.8],[.4,.05],[.5,.15],[.3,.7],[.4,.8],[.8,.4],[.9,.4],[.9,.2],[.75,.45]])).T
+X = np.concatenate((X1_blue.T, X1_red.T))
+Y = np.concatenate((Y1_0, Y1_1))
 
-X = np.concatenate((X1_0.T, X1_1.T))
-Y = np.concatenate((np.zeros((len(X1_0.T),1)), np.ones((len(X1_1.T),1))))
 
 """
 split_factor = 0.25
@@ -87,8 +25,8 @@ Y_train = Y[:-testSize]
 Y_test = Y[-testSize:]
 """
 
-x = np.arange(0, 1, 0.02)
-y = np.arange(0, 1, 0.02)
+x = np.arange(0, 1, .01)
+y = np.arange(0, 1, .01)
 xx, yy = np.meshgrid(x,y)
 
 grid = []
@@ -97,26 +35,26 @@ for i in xx[0]:
         grid.append([i,j[0]])
 
 plt.rcParams["figure.figsize"] = [10,8]
-plt.scatter(X1_0[0], X1_0[1], c="blue", s=75)
-plt.scatter(X1_1[0], X1_1[1], c="red", s=75)
-plt.pause(3)
+#plt.scatter(X1_blue[0], X1_blue[1], c="blue", s=75)
+#plt.scatter(X1_red[0], X1_red[1], c="red", s=75)
+#plt.pause(1)
 
-nn = NeuralNetwork(10)
-nn.train(X, Y, t=1000)
+nn = NeuralNetwork(6)
+nn.train(X, Y, t=100)
 
-for _ in range(100):
-    nn.trainOnline(X, Y, t=200)
+while(True):
+    nn.trainOnline(X, Y)
     pred = nn.predict(grid)
     color = []
     for p in pred:
-        if p.round() == 1:
+        if p[0] > p[1]:
             color.append("red")
         else:
             color.append("blue")
 
     plt.clf()
-    plt.scatter([grid[i][0] for i in range(len(grid)) if color[i]=="red"], [grid[i][1] for i in range(len(grid)) if color[i]=="red"], c="red", alpha=.4)
-    plt.scatter([grid[i][0] for i in range(len(grid)) if color[i]=="blue"], [grid[i][1] for i in range(len(grid)) if color[i]=="blue"], c="blue", alpha=.4)
-    plt.scatter(X1_0[0], X1_0[1], c="blue", s=75)
-    plt.scatter(X1_1[0], X1_1[1], c="red", s=75)
+    plt.scatter([grid[i][0] for i in range(len(grid)) if color[i]=="red"], [grid[i][1] for i in range(len(grid)) if color[i]=="red"], c="red", alpha=.1)
+    plt.scatter([grid[i][0] for i in range(len(grid)) if color[i]=="blue"], [grid[i][1] for i in range(len(grid)) if color[i]=="blue"], c="blue", alpha=.1)
+    plt.scatter(X1_blue[0], X1_blue[1], c="blue", s=75)
+    plt.scatter(X1_red[0], X1_red[1], c="red", s=75)
     plt.pause(0.05)
